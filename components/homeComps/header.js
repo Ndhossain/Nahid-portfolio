c85f1e45ae84031fabc0bde5400dce93d4c5193b
Ndhossain <hhhssnahiid@gmail.com>
2022-12-08 03:05:14 +0600

    Header section work on progress

--- components/homeComps/header.js
+++ components/homeComps/header.js
@@ -1,10 +1,25 @@
 import Image from "next/image";
-import myImage from '/assets/herosec1.jpg'
+import { BiCurrentLocation } from 'react-icons/bi';
+import { HiDocumentText } from 'react-icons/hi';
+import myImage from '/assets/herosec1.jpg';
 
 const Header = () => {
     return (
-        <div>
-            <div className="mt-10">
+        <div className="mt-10 flex justify-between">
+            <div>
+                <h1 className="text-2xl font-bold">Nahid Hossain</h1>
+                <div className="flex gap-5 mt-3">
+                    <p className="text-sm text-gray-500 dark:text-gray-400 flex gap-2 items-center">
+                        <BiCurrentLocation />Dinajpur, Bangladesh
+                    </p>
+                </div>
+                <div className="mt-3">
+                    <p className="text-gray-900 dark:text-white flex gap-2 items-center hover:underline text-lg">
+                        <HiDocumentText />Resume
+                    </p>
+                </div>
+            </div>
+            <div>
                 <Image className="rounded-full mx-auto" src={myImage} height={180} width={180} alt="Nahid" />
             </div>
         </div>
--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <div className='border-b-[1.5px] border-gray-900 dark:border-white sticky top-0 dark:bg-gray-900 bg-white'>
             <div className='max-w-[1024px] mx-auto px-5 py-3 flex justify-between items-center'>
                 <Link href='/' className='flex gap-2'>
-                    <h1 className='text-2xl font-bold dark:text-white'>Nahid Hossain</h1>
+                    <h1 className='text-xl font-bold dark:text-white'>Nahid Hossain</h1>
                 </Link>
                 <div className='flex gap-2 items-center'>
                     <ul className='flex gap-3 font-bold'>
