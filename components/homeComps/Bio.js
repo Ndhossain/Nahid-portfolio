8661ce17b51fb9faced5858459dd07de79cf0805
Ndhossain <hhhssnahiid@gmail.com>
2022-12-10 20:48:40 +0600

    header and bio work done

---
+++ components/homeComps/Bio.js
@@ -0,0 +1,25 @@
+import { AiOutlineMail } from 'react-icons/ai';
+import { BiCurrentLocation } from 'react-icons/bi';
+import { BsWhatsapp } from 'react-icons/bs';
+
+const Bio = () => {
+    return (
+        <div className='mt-20'>
+            <h1 className='text-3xl font-bold'>Bio</h1>
+            <div className="gap-5 mt-3">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum doloremque quos nam iusto similique tenetur accusamus ducimus enim harum nostrum corporis explicabo cumque reiciendis laborum reprehenderit voluptates architecto inventore veniam, placeat ut corrupti soluta distinctio. Quia tempora laboriosam beatae accusamus labore vitae deserunt.<br /><br />Nobis minima a libero sapiente vel facere maxime numquam autem praesentium, ex odio sequi, saepe, placeat nemo ducimus cumque! Dolorum harum cum magni aliquam? Similique laborum esse veritatis necessitatibus officiis quisquam dolorem sit ipsam, nam quaerat delectus deserunt minima soluta consectetur porro velit iste consequuntur pariatur! Eum vitae corrupti aliquid excepturi adipisci veniam architecto soluta tenetur pariatur modi repudiandae illum ad quo numquam itaque dolores, similique reprehenderit nulla perferendis? Esse ipsam magnam molestiae ab earum deserunt dicta quam necessitatibus repudiandae fugiat ullam dignissimos illum eaque libero, sed quos repellendus ducimus eveniet expedita natus, impedit temporibus vero vitae! Doloribus id facere nisi culpa hic nulla aut, rem consequatur obcaecati dolorem autem numquam, reprehenderit fugit sit provident molestiae vel? Numquam totam earum, itaque doloribus ipsum iure obcaecati vitae dolore nihil dolores veritatis ab nulla quia suscipit voluptas molestiae id esse fugit nobis optio ipsam reprehenderit maxime. A, quos incidunt doloribus et, recusandae earum distinctio obcaecati sit eius molestiae magnam!</p>
+                <p className="flex gap-2 items-center mt-10 text-lg font-bold">
+                    <BiCurrentLocation />Dinajpur, Bangladesh
+                </p>
+                <p className="flex gap-2 items-center mt-3 text-lg font-bold">
+                    <BsWhatsapp />+88 01724394004
+                </p>
+                <p className="flex gap-2 items-center mt-3 text-lg font-bold">
+                    <AiOutlineMail />2001.nahidhossain@gmail.com
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Bio;
--- components/homeComps/header.js
+++ components/homeComps/header.js
@@ -1,26 +1,60 @@
-import Image from "next/image";
-import { BiCurrentLocation } from 'react-icons/bi';
-import { HiDocumentText } from 'react-icons/hi';
-import myImage from '/assets/herosec1.jpg';
+import { AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { BsFileEarmarkPdf } from 'react-icons/bs';
+import { FaGithubSquare } from 'react-icons/fa';
+import { IoLogoFacebook } from 'react-icons/io';
+import { SiGmail } from 'react-icons/si';
 
 const Header = () => {
+    const openInNewTab = (url) => {
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
+    }
+
     return (
-        <div className="mt-10 flex justify-between">
+        <div className="mt-10">
             <div>
-                <h1 className="text-2xl font-bold">Nahid Hossain</h1>
-                <div className="flex gap-5 mt-3">
-                    <p className="text-sm text-gray-500 dark:text-gray-400 flex gap-2 items-center">
-                        <BiCurrentLocation />Dinajpur, Bangladesh
-                    </p>
-                </div>
+                <h1 className="text-3xl font-bold">Nahid Hossain</h1>
                 <div className="mt-3">
-                    <p className="text-gray-900 dark:text-white flex gap-2 items-center hover:underline text-lg">
-                        <HiDocumentText />Resume
+                    <div  onClick={() => openInNewTab("https://drive.google.com/file/d/15C2CW15IeQNBvNyAy8junyBCvcp2EHp1/view?usp=sharing")} className="text-gray-900 dark:text-white flex gap-2 items-center hover:underline text-lg cursor-pointer">
+                        <BsFileEarmarkPdf />Resume
+                    </div>
+                </div>
+                <div className='mt-10'>
+                    <p>
+                    Passionate Front-end/React developer. Love to take on challenges, explore new technologies, and build projects with them. Currently, working on my projects using the MERN stack. Good learner. Just Need proper resources and time to learn any technology. Seeking a job to use my skills and learn more about Web development.
                     </p>
                 </div>
-            </div>
-            <div>
-                <Image className="rounded-full mx-auto" src={myImage} height={180} width={180} alt="Nahid" />
+                <div className='mt-10'>
+                    <p className='flex gap-2 mt-3'>
+                        <span className='font-bold'>Expertise:</span> <span>JavaScript, React.js, Axios, React router, Tailwind, Bootstrap, Firebase, Stripe, HTML, CSS</span>
+                    </p>
+                    <p className='flex gap-2 mt-3'>
+                        <span className='font-bold'>Comfortable:</span> <span>React Query, Node.js, Express.js, MongoDB</span>
+                    </p>
+                    <p className='flex gap-2 mt-3'>
+                        <span className='font-bold'>Familiar:</span> <span>Next.js</span>
+                    </p>
+                    <p className='flex gap-2 mt-3'>
+                        <span className='font-bold'>Tools:</span> <span>Git, VsCode code editor</span>
+                    </p>
+                </div>
+                <div className='mt-10 flex gap-2'>
+                    <div className='hover:bg-white/20 rounded-full cursor-pointer p-2'>
+                        <IoLogoFacebook size={22} />
+                    </div>
+                    <div className='hover:bg-white/20 rounded-full cursor-pointer p-2'>
+                        <AiFillLinkedin size={22} />
+                    </div>
+                    <div className='hover:bg-white/20 rounded-full cursor-pointer p-2'>
+                        <FaGithubSquare size={22} />
+                    </div>
+                    <div className='hover:bg-white/20 rounded-full cursor-pointer p-2'>
+                        <AiFillInstagram size={22} />
+                    </div>
+                    <div className='hover:bg-white/20 rounded-full cursor-pointer p-2'>
+                        <SiGmail size={22} />
+                    </div>
+                </div>
             </div>
         </div>
     );
