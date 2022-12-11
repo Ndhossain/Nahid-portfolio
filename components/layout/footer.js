d9cf7d5f9c2f167cc612b233533ed94f194ac866
Ndhossain <hhhssnahiid@gmail.com>
2022-12-11 23:41:44 +0600

    basic footer work done

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,8 +1,10 @@
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className="mt-10 py-10 border-t-[1.5px] border-gray-900 dark:border-white">
+            <div className="max-w-[1024px] mx-auto px-5">
+                <p className="text-center">Copyright 2022 reserved by Nahid Hossain</p>
+            </div>
+        </footer>
     );
 };
 
