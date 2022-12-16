9cf3405d672b621b2bd94d77303e85102c20d85a
Ndhossain <hhhssnahiid@gmail.com>
2022-12-17 02:09:46 +0600

    almost done

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="mt-10 py-10 border-t-[1.5px] border-gray-900 dark:border-white">
+        <footer className="py-10 border-t-[1.5px] border-gray-900 dark:border-white">
             <div className="max-w-[1024px] mx-auto px-5">
                 <p className="text-center">Copyright 2022 reserved by Nahid Hossain</p>
             </div>
